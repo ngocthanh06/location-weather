d7e9f01546632d1e357176a607c2aaa1ba151816
= <=>
2020-06-10 13:19:13 +0700

    'fix'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname), '/dist'))
+app.use('/', serveStatic(path.join(__dirname)))
 
 app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'))
